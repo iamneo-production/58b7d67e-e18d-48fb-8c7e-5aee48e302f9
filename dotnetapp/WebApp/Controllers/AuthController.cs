55b45ed96c16bf051482d7999a1dbb871984dd28
sankeerthanasreeramula <sankeerthanasreeramula@gmail.com>
2023-06-13 06:52:25 +0000

    AuthController

---
+++ dotnetapp/WebApp/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+using System.Data.SqlClient;
+using System.Data;
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        BusinessLayer businesslayer = new BusinessLayer();
+        [HttpPost]
+        [Route("admin/signup")]
+        public string saveAdmin([FromBody] UserModel user)
+        {
+            return (businesslayer.saveAdmin(user));
+        }
+        [HttpPost]
+        [Route("admin/login")]
+        public Boolean isAdminPresent(LoginModel data)
+        {
+            return (businesslayer.isAdminPresent(data));
+        }
+        [HttpGet]
+        [Route("getAdminByEmailId")]
+        public UserModel getAdminByEmailId(string email)
+        {
+            return (businesslayer.getAdminByEmailId(email));
+        }
+        [HttpPost]
+        [Route("user/signup")]
+        public string saveUser(UserModel user)
+        {
+            return businesslayer.saveUser(user);
+        }
+        [HttpPost]
+        [Route("user/login")]
+        public Boolean isUserPresent(LoginModel data)
+        {
+            return businesslayer.isUserPresent(data);
+        }
+        [HttpGet]
+        [Route("getUserByEmailId")]
+        public UserModel getUserByEmailId(string email)
+        {
+            return businesslayer.getUserByEmailId(email);
+        }
+    }
+       
+}
+

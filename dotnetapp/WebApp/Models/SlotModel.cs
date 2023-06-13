184f11632798bb4454c89145c4a70c1666979ac5
sankeerthana_sreeramula <98333898+sankeerthanasreeramula@users.noreply.github.com>
2023-06-13 10:34:00 +0530

    Delete dotnetapp directory

--- dotnetapp/WebApp/Models/SlotModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApp.Models
-{
-    public class SlotModel
-    {
-        public string serviceCenterId { get; set; }
-        public DateTime appointmentDate { get; set; }
-        public List<string> Slots { get; set; }
-    }
-}

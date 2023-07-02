04cd88c55f2293f2f9ab478d8ca9d7684b22a85d
sankeerthanasreeramula <sankeerthanasreeramula@gmail.com>
2023-07-02 09:09:57 +0000

    dotnetapp and reactapp commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-import AddCenter from './Components/addCenter';
+import Signup from './components/Auth/Signup/Signup';
+import Login from './components/Auth/Login/Login';
+import AddCenter from './components/Admin/AddCenter/AddCenter';
+import Centerprofile from './components/Admin/Centerprofile/Centerprofile';
+import Service from './components/Admin/Service/Service';
+import Customers from './components/Admin/Customer/Customers';
+import HomePage from './components/Customer/HomePage/HomePage';
+import Dashboard from './components/Customer/Dashboard/Dashboard';
+import Booking from './components/Customer/Cart/Cart';
+import PrivateRoutes from './components/Auth/PrivateRoutes';
+import Review from './components/Admin/Review/Review';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+        <Route element={<PrivateRoutes />}>
+                <Route element={<AddCenter/>} path="/admin/AddCenter" exact/>
+                <Route element={<Centerprofile/>} path="/admin/Centerprofile" exact/>
+                <Route element={<Service/>} path="/admin/Service" exact/>
+                <Route element={<Customers/>} path="/admin/Customers" exact/>
+                <Route element={<Review/>} path="/admin/Review" exact/>
+                <Route element={<HomePage/>} path="/user/Homepage" exact/>
+                <Route element={<Dashboard/>} path="/user/Dashboard" exact/>
+                <Route element={<Booking/>} path="/user/Cart" exact/>
+            </Route>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

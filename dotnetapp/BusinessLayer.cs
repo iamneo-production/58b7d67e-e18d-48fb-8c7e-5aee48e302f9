9d8e38177b11d9e1b1a0f6be707ce68bfc88d502
NamrathaBudithi <namratha4452@gmail.com>
2023-07-04 21:31:00 +0000

    User

--- dotnetapp/BusinessLayer.cs
+++ dotnetapp/BusinessLayer.cs
@@ -18,132 +18,23 @@ namespace dotnetapp
         * It acts as an intermediary between the presentation layer (such as a user interface) and the data access layer. 
         * Its purpose is to encapsulate business logic and coordinate the interaction between the presentation layer and the data access layer. */
 
-        //Auth controllers
-        /*by creating an object for the data access layer, we are accessing all the methods */
-        public string saveAdmin(UserModel user)
-        {
-            return dataAccessLayer.saveAdmin(user);
-        }
-        public Boolean isAdminPresent(LoginModel data)
-        {
-            return (dataAccessLayer.isAdminPresent(data));
-        }
-        public UserModel getAdminByEmailId(string email)
-        {
-            return (dataAccessLayer.getAdminByEmailId(email));
-        }
-        public string saveUser(UserModel user)
-        {
-            return dataAccessLayer.saveUser(user);
-        }
-        public Boolean isUserPresent(LoginModel data)
-        {
-            return dataAccessLayer.isUserPresent(data);
-        }
-        public UserModel getAdminByEmailId(string email)
-        {
-            return (dataAccessLayer.getAdminByEmailId(email));
-        }
-        public UserModel getUserByEmailId(string email)
-        {
-            return dataAccessLayer.getUserByEmailId(email);
-        }
-
         //User Controller
         /*by creating an object for the data access layer, we are accessing all the methods */
-        
-        public string editUsersById(UserModel user, int UserId)
+        public string addUser(UserModel user)
         {
-            return dataAccessLayer.editUsersById(user, UserId);
+            return dataAccessLayer.addUser(user);
         }
-
-        //AppointmentController
-        /*by creating an object for the data access layer, we are accessing all the methods */
-        public List<AppointmentModel> getSlotDetailsByDate(string serviceCenterId, DateTime Date)
+        public List<UserModel> getAllUsers()
         {
-            return dataAccessLayer.getSlotDetailsByDate(serviceCenterId, Date);
+            return dataAccessLayer.getAllUsers();
         }
-        public string saveAppointment(ProductModel data)
+        public UserModel getUser(int UserId)
         {
-            return dataAccessLayer.saveAppointment(data);
+            return dataAccessLayer.getUser(UserId);
         }
-        public string postAvailableSlots(AppointmentModel model)
+        public string deleteUsers(List<int> userIds)
         {
-            return dataAccessLayer.postAvailableSlots(model);
-        }
-        public List<ProductModel> getAppointment(string email)
-        {
-            return dataAccessLayer.getAppointment(email);
-        }
-        public ProductModel getAppointmentSlotsById(int id)
-        {
-            return dataAccessLayer.getAppointmentSlotsById(id);
-        }
-        public string EditAppointment(int ID, [FromBody] ProductModel model)
-        {
-            return dataAccessLayer.EditAppointment(ID, model);
-        }
-        public string updateOnDeleteAppointment(AppointmentModel model)
-        {
-            return dataAccessLayer.updateOnDeleteAppointment(model);
-        }
-        public string deleteAppointment(int ID)
-        {
-            return dataAccessLayer.deleteAppointment(ID);
-        }
-        public List<ProductModel> getAllAppointments()
-        {
-            return dataAccessLayer.getAllAppointments();
-        }
-
-        //servicecentercontroller
-        /*by creating an object for the data access layer, we are accessing all the methods */
-        public string availableSlots(AppointmentModel m)
-        {
-            return dataAccessLayer.availableSlots(m);
-        }
-        public List<ServiceCenterModel> viewServiceCenter()
-        {
-            return dataAccessLayer.viewServiceCenter();
-        }
-        public ServiceCenterModel viewServiceCenterByID(string serivceCenterId)
-        {
-            return dataAccessLayer.viewServiceCenterByID(serivceCenterId);
-        }
-        public string updateGetSlots(string serviceCenterId, AppointmentModel model)
-        {
-            return dataAccessLayer.updateGetSlots(serviceCenterId, model);
-        }
-        public string deleteServiceCenter(string serivceCenterId)
-        {
-            return dataAccessLayer.deleteServiceCenter(serivceCenterId);
-        }
-        public string deleteAvailableSlots(string serviceCenterId)
-        {
-            return dataAccessLayer.deleteAvailableSlots(serviceCenterId);
-        }
-        public string addServiceCenter([FromBody] JsonElement jsonData)
-        {
-            return dataAccessLayer.addServiceCenter(jsonData);
-        }
-        public string editServiceCenter(string serviceCenterId, [FromBody] JsonElement jsonData)
-        {
-            return dataAccessLayer.editServiceCenter(serviceCenterId, jsonData);
-        }
-
-        //Review Controller
-        /*by creating an object for the data access layer, we are accessing all the methods */
-        public string AddReview(ReviewModel model)
-        {
-            return dataAccessLayer.AddReview( model);
-        }
-        public ReviewModel getReviews(string id)
-        {
-            return dataAccessLayer.getReviews(id);
-        }
-        public List<ReviewModel> getAllReviews()
-        {
-            return dataAccessLayer.getAllReviews();
+            return dataAccessLayer.deleteUsers(userIds);
         }
     }
  }
--- dotnetapp/Controllers/UserController.cs
+++ dotnetapp/Controllers/UserController.cs
@@ -20,12 +20,39 @@ namespace dotnetapp.Controllers
 
         BusinessLayer businesslayer = new BusinessLayer();
 
-        /* this method returns an IActionResult, and the result being returned is a string */
-        [HttpPut]
-        [Route("editUsersById/{UserId}")]
-        public IActionResult editUsersById(UserModel user, int UserId)
+        [HttpPost]
+        [Route("addUser")]
+        public IActionResult addUser(UserModel user)
         {
-            string result = businesslayer.editUsersById(user, UserId);
+            string result = businesslayer.addUser(user);
+            return Ok(result);
+        }
+
+        /* this method returns an IActionResult, and the result being returned is a List */
+        [HttpGet]
+        [Route("getAllUsers")]
+        public IActionResult getAllUsers()
+        {
+            List<UserModel> result = businesslayer.getAllUsers();
+            return Ok(result);
+        }
+
+        /* this method returns an IActionResult, and the result being returned is a Model */
+        [HttpGet]
+        [Route("getUser/{UserId}")]
+
+        public IActionResult getUser(int UserId)
+        {
+            UserModel result = businesslayer.getUser(UserId);
+            return Ok(result);
+        }
+
+        /* this method returns an IActionResult, and the result being returned is a string */
+        [HttpDelete]
+        [Route("deleteUsers")]
+        public IActionResult deleteUsers(List<int> userIds)
+        {
+            string result = businesslayer.deleteUsers(userIds);
             return Ok(result);
         }
 

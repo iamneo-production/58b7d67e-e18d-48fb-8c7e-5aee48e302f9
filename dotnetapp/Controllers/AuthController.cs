1e5d4c8a2c785f1af11c44957158b26c23595b82
AlekyaRekapalli <alekyarekapalli@gmail.com>
2023-07-06 09:58:38 +0000

    Auth

--- dotnetapp/Controllers/AuthController.cs
+++ dotnetapp/Controllers/AuthController.cs
@@ -9,7 +9,6 @@ using dotnetapp.Models;
 namespace dotnetapp.Controllers
 {
     /*This class Control the user /admin signup and signin*/
-    //[Route("api/[controller]")]
     [ApiController]
     public class AuthController : ControllerBase
     {
@@ -47,24 +46,6 @@ namespace dotnetapp.Controllers
         public IActionResult isUserPresent(LoginModel data)
         {
             return Created("UserLogin",businesslayer.isUserPresent(data));
-        }
-
-          /* this method returns an IActionResult, and the result being returned is a string */
-        [HttpGet]
-        [Route("getAdminByEmailId")]
-        public IActionResult getAdminByEmailId(string email)
-        {
-            UserModel result = businesslayer.getAdminByEmailId(email);
-            return Ok(result);
-        }
-
-        /* this method returns an IActionResult, and the result being returned is a Model */
-        [HttpGet]
-        [Route("getUserByEmailId")]
-        public IActionResult getUserByEmailId(string email)
-        {
-            UserModel result = businesslayer.getUserByEmailId(email);
-            return Ok(result);
         }
     }
 }

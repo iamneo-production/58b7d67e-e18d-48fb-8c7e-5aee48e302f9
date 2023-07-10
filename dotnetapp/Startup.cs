101365101dbe44510259d12c87a3fc38acf1263c
Manoj-Deva <manojdeva85@gmail.com>
2023-07-10 06:30:20 +0000

    final commit

--- dotnetapp/Startup.cs
+++ dotnetapp/Startup.cs
@@ -31,7 +31,15 @@ namespace dotnetapp
             //string connectionString = Configuration.GetConnectionString("myconnstring");
            // services.AddDbContext<ProductDBContext>(opt => opt.UseSqlServer(connectionString));
            // services.AddScoped<IProductService, ProductService>();
-            services.AddCors();
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                });
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -53,6 +61,8 @@ namespace dotnetapp
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 

5b9eb4bbee857b446d095adad6e7126e2afc4296
nizam9519 <20225a0232@bvcgroup.in>
2023-07-12 06:08:49 +0000

    models added

---
+++ dotnetapp/Models/ProductModel.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApi.Models
+{
+    public class ProductModel
+    {
+        public int ID { get; set; }
+        public string customerName { get; set; }
+        public string productName { get; set; }
+        public string productModelNo { get; set; }
+        public DateTime dateofPurchase { get; set; }
+        public string contactNumber { get; set; }
+        public string problemDescription { get; set; }
+        public string bookedSlots { get; set; }
+        public DateTime dateOfAppointment { get; set; }
+        public string email { get; set; }
+        public string serviceCenterId { get; set; }
+        public string serviceCenterName { get; set; }
+
+        public DateTime dateOfAppointmentBooking { get; set; }
+        public string serviceCost { get; set; }
+    }
+}
